fbc63cddae0f85beeb341b4ed8dc77c9a384a5c2
Derek Milne <derek.m.m.77@gmail.com>
2024-02-14 15:45:10 -0800

    isNil func

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-function isNumber(x: unknown): x is number {
+import { Nil } from "./types";
+
+export function isNumber(x: unknown): x is number {
   return typeof x === "number" && !isNaN(x);
+}
+
+export function isNil(x: unknown): x is Nil {
+  return x === null || x === undefined;
 }
