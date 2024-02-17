1d64491acd7adabaf936cff2cab8e5e6d841e494
Derek Milne <derek.m.m.77@gmail.com>
2024-02-16 16:08:55 -0800

    add isArray and isObject methods

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,11 @@ export function isNumber(x: unknown): x is number {
 export function isNil(x: unknown): x is Nil {
   return x === null || x === undefined;
 }
+
+export function isArray(x: unknown): x is Array<unknown> {
+  return Array.isArray(x);
+}
+
+export function isObject(x: unknown): x is Object {
+  return !isNil(x) && typeof x === "object" && !isArray(x);
+}
